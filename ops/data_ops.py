1f2de4bdb3e612b8d86a4ed7152e2634de913846
Cameron Fabbri <cameronfabbri@gmail.com>
2017-11-17 12:00:53 -0600

    loading galaxy data now

--- ops/data_ops.py
+++ ops/data_ops.py
@@ -75,6 +75,69 @@ def get_attr_name(attr):
    
    return s
 
+
+'''
+
+   Loading up the galaxy dataset. Only considering the following:
+   
+   1  T               - EFIGI morphological type
+   7  Arm strength    - Strength of spiral arms
+   10 Arm curvature   - Average curvature of the spiral arms
+   13 Arm Rotation    - Direction of the winding of the spiral arms
+   16 Bar length      - Length of the central bar
+   19 Inner Ring      - Strength of the inner ring, lens or inner pseudo-ring
+   22 Outer Ring      - Strength of outer ring
+   25 Pseudo Ring     - Type and strength of outer pseudo-ring
+   28 Perturbation    - Deviation from rotational symmetry
+   31 Visible Dust    - Strength of dust features
+   34 Dust Dispersion - Patchiness of dust features
+   40 Hot Spots       - Strength of regions of strong star formation, active nuclei, or stellar nuclie
+   49 Multiplicity    - Abundance of neighbouring galaxies
+
+'''
+def load_galaxy(data_dir):
+
+   idx = np.array([0, 1, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 40, 49])
+
+   train_images     = glob.glob(data_dir+'images/train/*.png')
+   test_images      = glob.glob(data_dir+'images/test/*.png')
+
+   # get train ids from train folder
+   train_ids = [ntpath.basename(x.split('.')[0]) for x in train_images]
+   test_ids  = [ntpath.basename(x.split('.')[0]) for x in test_images]
+
+   iptr = data_dir+'images/train/'
+   ipte = data_dir+'images/test/'
+
+   train_images = []
+   test_images  = []
+   train_attributes = []
+   test_attributes  = []
+
+   with open(data_dir+'EFIGI_attributes.txt', 'r') as f:
+      for line in f:
+         line     = line.rstrip().split()
+         image_id = line[0]
+         line     = np.asarray(line[1:])
+         line     = line[idx].astype('float32')
+         if image_id in train_ids:
+            img = misc.imread(iptr+image_id+'.png').astype('float32')
+            img = misc.imresize(img, (64, 64))
+            train_images.append(img)
+            train_attributes.append(line)
+         elif image_id in test_ids:
+            img = misc.imread(ipte+image_id+'.png').astype('float32')
+            img = misc.imresize(img, (64, 64))
+            test_images.append(img)
+            test_attributes.append(line)
+
+   train_images = np.asarray(train_images)
+   test_images = np.asarray(test_images)
+   train_attributes = np.asarray(train_attributes)
+   test_attributes = np.asarray(test_attributes)
+
+   return train_images, train_attributes, test_images, test_attributes
+
 def load_mnist(data_dir, mode='train'):
 
    url = 'http://deeplearning.net/data/mnist/mnist.pkl.gz'
